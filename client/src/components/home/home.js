b4a27114770707f5cea19159886f03e66d7942f8
Jashcraft <ashcraft.jonathon@gmail.com>
2025-03-20 15:43:10 -0600

    adjust main photo vh

--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -13,6 +13,7 @@ export default function HomePage() {
           src={require("../../images/WTW_Main_Photo.jpg")}
           width={"100%"}
           alt=""
+          style={{minHeight:"80vh"}}
         />
         <img
           src={require("../../images/WTW_Font_Logo.PNG")}
