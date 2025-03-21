0daee1fabedb2569e30475d494df70041a68855b
Jashcraft <ashcraft.jonathon@gmail.com>
2025-03-20 18:12:43 -0600

    add media section

--- client/src/components/home/home-about.js
+++ client/src/components/home/home-about.js
@@ -1,19 +1,29 @@
 import { Box } from "@mui/material";
 import * as React from "react";
-import "./home.css"
+import "./home.css";
 
-export default function HomeAbout () {
-
-
+export default function HomeAbout() {
   return (
     <div>
       <Box>
-        <h3
-        className="centered"
-        style={{color: "white"}}
-        >ABOUT US</h3>
+        <h3 className="centered" style={{ color: "white" }}>
+          ABOUT US
+        </h3>
+        <div className="centered">
+          <div className="centered" style={{ color: "white", width: "75%" }}>
+            <p>
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum."
+            </p>
+          </div>
+        </div>
       </Box>
       <hr style={{ width: "75%" }} />
     </div>
-  )
+  );
 }
--- client/src/components/home/home-music.js
+++ client/src/components/home/home-music.js
@@ -1,19 +1,56 @@
-import { Box } from "@mui/material";
+import { Box, Card, Divider, Grid2, Stack } from "@mui/material";
 import * as React from "react";
-import "./home.css"
+import "./home.css";
 
-export default function HomeMusic () {
-
-
+export default function HomeMusic() {
   return (
     <div>
       <Box>
-        <h3
-        className="centered"
-        style={{color: "white"}}
-        >HEAR OUR MUSIC</h3>
+        <h3 className="centered" style={{ color: "white" }}>
+          HEAR OUR MUSIC
+        </h3>
+        <Box className="centered">
+          <Grid2 container spacing={2} sx={{width:"75%"}}>
+            <Grid2 size={{s: 12, md: 4}}>
+              <Card >
+                <iframe
+                  src="https://www.youtube.com/embed/nOmdvVXOnhw?si=9DAIPYNVVuiCY00-"
+                  title="YouTube video player"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  referrerPolicy="strict-origin-when-cross-origin"
+                  allowFullScreen
+                  height={"auto"}
+                ></iframe>
+              </Card>
+            </Grid2>
+            <Grid2 size={{s: 12, md: 4}}>
+              <Card >
+                <iframe
+                  src="https://www.youtube.com/embed/NQFbxpH8pEE?si=az3f17K0z8uFR5gN"
+                  title="YouTube video player"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  referrerPolicy="strict-origin-when-cross-origin"
+                  allowFullScreen
+                  height={"auto"}
+                ></iframe>
+              </Card>
+            </Grid2>
+            <Grid2 size={{s: 12, md: 4}}>
+              <Card >
+                <iframe
+                  src="https://www.youtube.com/embed/sXhlzyeR06Q?si=vnyA8CTayK9Lnti7"
+                  title="YouTube video player"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  referrerPolicy="strict-origin-when-cross-origin"
+                  allowFullScreen
+                  height={"auto"}
+                ></iframe>
+              </Card>
+            </Grid2>
+          </Grid2>
+        </Box>
       </Box>
       <hr style={{ width: "75%" }} />
     </div>
-  )
+  );
 }
